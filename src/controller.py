4653a67bc77680214e7b780f4eceb3d32e6c4d61
shrutya22487 <shrutya22487@iiitd.ac.in>
2025-09-29 10:51:08 +0530

    added server state

--- src/controller.py
+++ src/controller.py
@@ -1,23 +1,23 @@
 from collections import deque
 import numpy as np
+import pandas as pd
+import agent
 import random
 import time
 import math
 import servers
 import user
 from typing import List
-import agent
-import pandas as pd
 
-# TODO (@BingoBoy479)(@theshamiksinha) Resolve comments here
 class Controller:
-    def __init__(self, requests : List[user.Request]):
+    def __init__(self, requests : List[user.Request], input_dim=10, output_dim=5):
         self.requests = requests
         # add encoder here
         # add any other initialization you need
         self.agent = agent.DQNAgent()
+        self.encoder = agent.Encoder(input_dim, output_dim)
+        self.server_state = pd.read_csv('data/server_state.csv').iloc[:, 1:6]
     
-    # TODO (@BingoBoy479): Add first 5 columns of the server_state.csv to the state as well
     def _merge_server_state_with_request(self):
         """
         Reads server_data.csv and assigns a random process_id to each request.
@@ -31,12 +31,13 @@ class Controller:
                 req.process_id = random.choice(detect_indices)
                 req.cpu_usage = df[req.process_id]['CPU Usage']
                 req.ram_usage = df[req.process_id]['RAM Usage']
+                req.server_state = self.server_state.sample(n=1).iloc[0].tolist()
         else:
             raise ValueError("No entries found with 'Current Script' as 'detect.py'")
     
     def run(self):
         self._merge_server_state_with_request()
-        # add encoder here to and connect with agent here and pass the requests to it
+        #connect with contoller here and pass the requests to it
         
         
     
--- src/user.py
+++ src/user.py
@@ -2,9 +2,10 @@
 class Request:
     def __init__(self, request_id):
         self.request_id = request_id
-        self.process_id = None
-        self.ram_usage = None
-        self.cpu_usage = None
+        self.process_id = 0
+        self.ram_usage = 0
+        self.cpu_usage = 0
+        self.server_state = []
 
 # TODO(@BingoBoy479): Add state here or a corresponding class edit in all other files accordingly
 class Users:
